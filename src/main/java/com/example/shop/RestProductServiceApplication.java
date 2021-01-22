3ec168a368cf023256885ab515e59c24bba9b236
Admin <uscmagnus@gmail.com>
2021-01-22 22:40:08 +0200

    Initial commit

---
+++ src/main/java/com/example/shop/RestProductServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.shop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestProductServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RestProductServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/shop/RestProductServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.shop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestProductServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
